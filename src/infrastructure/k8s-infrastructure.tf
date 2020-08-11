7eac127708593fcef09d8c149b81cf4e42e1ef2a
tonylunt <tonyjlunt@gmail.com>
2020-08-11 15:50:33 -0400

    outputs

--- src/infrastructure/k8s-infrastructure.tf
+++ src/infrastructure/k8s-infrastructure.tf
@@ -23,37 +23,37 @@ module "aks" {
 
 }
 
-module "gke" {
-  source                      = "app.terraform.io/cardinalsolutions/kubernetes-engine/google//modules/beta-public-cluster"
-  version                     = "9.2.0"
-  project_id                  = local.cloud_config.gke.project_id
-  name                        = "${local.common_name}${random_id.suffix.hex}"
-  region                      = local.cloud_config.gke.location
-  network                     = google_compute_network.main.name
-  subnetwork                  = google_compute_subnetwork.main.name
-  ip_range_pods               = google_compute_subnetwork.main.name
-  ip_range_services           = google_compute_subnetwork.main.secondary_ip_range.range_name
+# module "gke" {
+#   source                      = "app.terraform.io/cardinalsolutions/kubernetes-engine/google//modules/beta-public-cluster"
+#   version                     = "9.2.0"
+#   project_id                  = local.cloud_config.gke.project_id
+#   name                        = "${local.common_name}${random_id.suffix.hex}"
+#   region                      = local.cloud_config.gke.location
+#   network                     = google_compute_network.main.name
+#   subnetwork                  = google_compute_subnetwork.main.name
+#   ip_range_pods               = google_compute_subnetwork.main.name
+#   ip_range_services           = google_compute_subnetwork.main.secondary_ip_range.range_name
   
-  # Disable workload identity
-  identity_namespace = null
-  node_metadata      = "UNSPECIFIED"
-}
+#   # Disable workload identity
+#   identity_namespace = null
+#   node_metadata      = "UNSPECIFIED"
+# }
 
-resource "google_compute_network" "main" {
-  name        = "demo"
-  description = "demo network"
+# resource "google_compute_network" "main" {
+#   name        = "demo"
+#   description = "demo network"
 
-  auto_create_subnetworks = false
-}
+#   auto_create_subnetworks = false
+# }
 
-resource "google_compute_subnetwork" "main" {
-  name          = "subnet-1a"
-  region        = local.cloud_config.gke.location
-  ip_cidr_range = "192.168.5.0/24"
-  network       = google_compute_network.main.self_link
+# resource "google_compute_subnetwork" "main" {
+#   name          = "subnet-1a"
+#   region        = local.cloud_config.gke.location
+#   ip_cidr_range = "192.168.5.0/24"
+#   network       = google_compute_network.main.self_link
 
-  secondary_ip_range {
-    range_name    = "subnet-1b"
-    ip_cidr_range = "192.168.6.0/24"
-  }
-}
+#   secondary_ip_range {
+#     range_name    = "subnet-1b"
+#     ip_cidr_range = "192.168.6.0/24"
+#   }
+# }
