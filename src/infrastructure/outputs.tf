c58c7a023f590282b7d5f5312fb15427f3215744
tonylunt <tonyjlunt@gmail.com>
2020-08-11 15:51:29 -0400

    Fixing output name

--- src/infrastructure/outputs.tf
+++ src/infrastructure/outputs.tf
@@ -4,6 +4,6 @@ output "aks" {
          host = module.aks.fqdn
          client_certificate     = module.aks.admin_client_certificate
          client_key             = module.aks.admin_client_key
-         cluster_ca_certificate = module.aks_admin_cluster_ca_certificate
+         cluster_ca_certificate = module.aks.admin_cluster_ca_certificate
     }
 }
