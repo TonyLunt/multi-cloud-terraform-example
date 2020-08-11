576b5909571ccdff1bae84506fc13e512fb62de6
tonylunt <tonyjlunt@gmail.com>
2020-08-11 17:00:09 -0400

    commenting out outputs

--- src/infrastructure/outputs.tf
+++ src/infrastructure/outputs.tf
@@ -1,9 +1,9 @@
-output "aks" {
-    sensitive   = true
-    value = {
-         host = module.aks.fqdn
-         client_certificate     = module.aks.admin_client_certificate
-         client_key             = module.aks.admin_client_key
-         cluster_ca_certificate = module.aks.admin_cluster_ca_certificate
-    }
-}
+# output "aks" {
+#     sensitive   = true
+#     value = {
+#          host = module.aks.fqdn
+#          client_certificate     = module.aks.admin_client_certificate
+#          client_key             = module.aks.admin_client_key
+#          cluster_ca_certificate = module.aks.admin_cluster_ca_certificate
+#     }
+# }
