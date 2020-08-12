b388eea7f9ddd24a9a8c5645958a998512be8910
tonylunt <tonyjlunt@gmail.com>
2020-08-12 12:42:49 -0400

    re-enabling outputso n aks cluster

--- src/infrastructure/local-modules/aks/outputs.tf
+++ src/infrastructure/local-modules/aks/outputs.tf
@@ -79,23 +79,23 @@ output "admin_password" {
 }
 
 
-# kube_config
-# output "client_key" {
-#   description = "Base64 encoded private key used by clients to authenticate to the Kubernetes cluster."
-#   value       = azurerm_kubernetes_cluster.module.kube_config.0.client_key
-# }
+kube_config
+output "client_key" {
+  description = "Base64 encoded private key used by clients to authenticate to the Kubernetes cluster."
+  value       = azurerm_kubernetes_cluster.module.kube_config.0.client_key
+}
 
 
-# output "client_certificate" {
-#   description = "Base64 encoded public certificate used by clients to authenticate to the Kubernetes cluster."
-#   value       = azurerm_kubernetes_cluster.module.kube_config.0.client_certificate
-# }
+output "client_certificate" {
+  description = "Base64 encoded public certificate used by clients to authenticate to the Kubernetes cluster."
+  value       = azurerm_kubernetes_cluster.module.kube_config.0.client_certificate
+}
 
 
-# output "cluster_ca_certificate" {
-#   description = "Base64 encoded public CA certificate used as the root of trust for the Kubernetes cluster."
-#   value       = azurerm_kubernetes_cluster.module.kube_config.0.cluster_ca_certificate
-# }
+output "cluster_ca_certificate" {
+  description = "Base64 encoded public CA certificate used as the root of trust for the Kubernetes cluster."
+  value       = azurerm_kubernetes_cluster.module.kube_config.0.cluster_ca_certificate
+}
 
 
 output "host" {
--- src/infrastructure/outputs.tf
+++ src/infrastructure/outputs.tf
@@ -2,16 +2,16 @@ output "aks" {
     sensitive   = true
     value = {
          host = module.aks.fqdn
-         client_certificate     = module.aks.admin_client_certificate
-         client_key             = module.aks.admin_client_key
-         cluster_ca_certificate = module.aks.admin_cluster_ca_certificate
+         client_certificate     = module.aks.client_certificate
+         client_key             = module.aks.client_key
+         cluster_ca_certificate = module.aks.cluster_ca_certificate
     }
 }
 
 output "gke" {
     sensitive   = true
     value = {
-         host = module.aks.fqdn
+         host = module.gke.fqdn
          client_certificate     = data.google_container_cluster.gke.master_auth.0.client_certificate
          client_key             = data.google_container_cluster.gke.master_auth.0.client_key
          cluster_ca_certificate = data.google_container_cluster.gke.master_auth.0.cluster_ca_certificate
